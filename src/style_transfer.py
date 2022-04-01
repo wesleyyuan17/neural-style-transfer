2b768ea54a74460a9997a3a316f587c06e3e66fb
wesleyyuan17 <wesley.yuan1746@gmail.com>
2022-04-01 15:25:01 -0400

    added use of gpu to main script

--- src/style_transfer.py
+++ src/style_transfer.py
@@ -18,46 +18,67 @@ from util import *
 
 
 def train(output_image, target_style_representation, target_content_representation, feature_maps, model, optimizer, n_steps, a, b):
-    for iter in range(n_steps):
-        # get feature maps of current output image
-        _ = model(output_image)
-        output_content_representation = get_content_representation(feature_maps.copy())
-        output_style_representation = get_style_representation(feature_maps.copy())
+    if isinstance(optimizer, optim.SGD):
+        for iter in range(n_steps):
+            # get feature maps of current output image
+            _ = model(output_image)
+            output_content_representation = get_content_representation(feature_maps.copy())
+            output_style_representation = get_style_representation(feature_maps.copy())
 
-        # calculate errors and backprop
-        loss = a * content_loss(target_content_representation, output_content_representation) + b * style_loss(target_style_representation, output_style_representation)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # calculate errors and backprop
+            loss = a * content_loss(target_content_representation, output_content_representation) + b * style_loss(target_style_representation, output_style_representation)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    elif isinstance(optimizer, optim.LBFGS):
+        def closure():
+            if torch.is_grad_enabled():
+                optimizer.zero_grad()
+
+            _ = model(output_image)
+            output_content_representation = get_content_representation(feature_maps.copy())
+            output_style_representation = get_style_representation(feature_maps.copy())
+
+            loss = a * content_loss(target_content_representation, output_content_representation) + b * style_loss(target_style_representation, output_style_representation)
+            if loss.requires_grad:
+                loss.backward()
+                
+            return loss
+
+        for iter in range(n_steps):
+            optimizer.step(closure)
     
     return output_image
 
+def main(style_image, content_image, n_steps, lr, optimizer, a=1e-3, b=0.1):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def main(style_image, content_image, n_steps, a=0.1, b=1e-3):
     # for saving model later
     style_name = style_image.split('.')[0]
     content_name = content_image.split('.')[0]
 
     # load images and models
     style_image = read_image('../style_images/{}'.format(style_image)).transpose([2, 0, 1])
-    content_image = torch.Tensor(read_image('../content_images/{}'.format(content_image)).transpose([2, 0, 1]))
+    content_image = torch.Tensor(read_image('../content_images/{}'.format(content_image)).transpose([2, 0, 1])).unsqueeze(0)
     style_image = torch.Tensor(transform.resize(style_image, content_image.shape))
     content_image = content_image / content_image.max()
 
     # get white noise image to run gradient descent on for output image
-    # output_image = Variable(torch.rand(content_image.shape), requires_grad=True)
-    output_image = Variable(content_image.detach().clone(), requires_grad=True)
+    output_image = torch.rand(content_image.shape, device=device, requires_grad=True)
+    # output_image = Variable(content_image.detach().clone(), requires_grad=True)
 
     model = vgg19(pretrained=True).features # only want feature maps, don't need classifier portion
     model.eval()
+    model.to(device)
 
     # freeze parameters so transfer learning gradient descent just affects images, doesn't retrain network
     for param in model.parameters():
         param.requires_grad = False
 
-    # summary(model, INPUT_SIZE)
-    # print(model)
+    # send targets to proper device
+    content_image.to(device)
+    style_image.to(device)
 
     # get style/content representations which are static for a given model/image
     feature_maps = set_hooks(model)
@@ -66,7 +87,10 @@ def main(style_image, content_image, n_steps, a=0.1, b=1e-3):
     _ = model(style_image)
     target_style_representation = get_style_representation({k: v.detach() for k, v in feature_maps.items()})
 
-    optimizer = optim.SGD([output_image], lr=1e-2, momentum=0.9)
+    if optimizer == 'sgd':
+        optimizer = optim.SGD([output_image], lr=lr, momentum=0.9)
+    elif optimizer == 'lbfgs':
+        optimizer = optim.LBFGS([output_image], lr=lr)
     output_image = train(output_image, 
                          target_style_representation, 
                          target_content_representation, 
@@ -93,6 +117,10 @@ if __name__ == '__main__':
                         help='The path image from which content of new image should be taken')
     parser.add_argument('--n_steps', dest='n_steps', required=True,
                         help='The number of training steps to take in generating final image')
+    parser.add_argument('--lr', dest='lr', required=True,
+                        help='Learning rate for optimizer')
+    parser.add_argument('--optimizer', dest='optimizer', required=True,
+                        help='Which pytorch optimizer to use')
     args = parser.parse_args()
 
-    main(args.style_image, args.content_image, int(args.n_steps))
+    main(args.style_image, args.content_image, int(args.n_steps), float(args.lr), args.optimizer)
